3dd607ef5d980dcce55ef185a426baff420417ec
tralesmile <trajce_2000@hotmail.com>
2023-03-28 20:19:54 +0200

    Restructure Project

---
+++ TajmautMK.API/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using TajmautMK.Common.Models.ModelsREQUEST;
+
+namespace TajmautMK.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class AdminController : ControllerBase
+    {
+
+        [HttpPut("ChangeUserRole")]
+        public async Task<ActionResult> ChangeUserRole(UserRoleREQUEST request)
+        {
+            return Ok(request);
+        }
+
+    }
+}
